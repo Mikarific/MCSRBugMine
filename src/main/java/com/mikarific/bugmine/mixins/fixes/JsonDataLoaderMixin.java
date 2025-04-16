10fc044efccca99005dd2920e929c8b7ad8124c9
Mikarific <mikerific2@gmail.com>
2025-04-16 17:07:55 -0600

    Make "In It Together" inherent the conditions of the original.

--- src/main/java/com/mikarific/bugmine/mixins/fixes/JsonDataLoaderMixin.java
+++ src/main/java/com/mikarific/bugmine/mixins/fixes/JsonDataLoaderMixin.java
@@ -4,6 +4,7 @@ import com.mikarific.bugmine.config.Config;
 import net.minecraft.advancement.Advancement;
 import net.minecraft.advancement.AdvancementCriterion;
 import net.minecraft.advancement.AdvancementDisplay;
+import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.advancement.criterion.TickCriterion;
 import net.minecraft.resource.JsonDataLoader;
 import net.minecraft.resource.ResourceManager;
@@ -33,7 +34,7 @@ public abstract class JsonDataLoaderMixin<T> extends SinglePreparationResourceRe
                 AdvancementDisplay fixedDisplay = new AdvancementDisplay(display.getIcon(), display.getTitle(), Text.translatable("bugmine.advancements.unlocks.in_it_together.description"), Text.translatable("bugmine.advancements.unlocks.in_it_together.description"), display.getBackground(), display.getFrame(), display.shouldShowToast(), display.shouldAnnounceToChat(), display.isHidden());
                 fixedDisplay.setPos(display.getX(), display.getY());
 
-                Map<String, AdvancementCriterion<?>> fixedCriteria = Map.of("complete_level", TickCriterion.Conditions.createLevelCompleted());
+                Map<String, AdvancementCriterion<?>> fixedCriteria =  Map.of("complete_level", Criteria.LEVEL_COMPLETED.create((TickCriterion.Conditions) inItTogether.criteria().get("complete_level").conditions()));
 
                 Advancement fixedAdvancement = new Advancement(inItTogether.parent(), Optional.ofNullable(fixedDisplay), inItTogether.rewards(), fixedCriteria, inItTogether.requirements(), inItTogether.sendsTelemetryEvent());
                 advancements.replace(new Identifier("minecraft", "unlocks/in_it_together"), fixedAdvancement);
